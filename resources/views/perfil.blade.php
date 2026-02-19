cc084f0f4113a6f24642d5a790c7e52ec3919910
Tomas Urieles <tomasurieles31@gmail.com>
2026-02-18 22:03:37 -0500

    vista perfil.blade.php creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<body>
+    <div class="perfil-card">
+        <h1>PERFIL</h1>
+        <div class="perfil-contenido">
+            <h2>Nombre: Tomas Enrique Urieles Chuscano</h2>
+            <h3>Edad: 19</h3>
+            <h3>Universidad: Unab</h3>
+            <h3>Semestre: Quinto</h3>
+        </div>
+    </div>
+</body>
+</html>
