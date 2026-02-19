a413ddd887278af8bb1e13c9214cb8f4fcf3cdee
Tomas Urieles <tomasurieles31@gmail.com>
2026-02-18 22:19:45 -0500

    vista metas.blade.php

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Metas</title>
+</head>
+<body>
+    <h1> METAS</h1>
+    <h3>Corto plazo:</h3>
+    <ul><li>Tener una mejor comprensión de Laravel</li></ul>
+    <ul><li>Terminar la carrera</li></ul>
+    <ul><li>Montar un negocio de camisas </li></ul>
+    <h3>Largo plazo:</h3>
+    <ul><li>Ejercer mi profesión</li></ul>
+    <ul><li>Salir del país</li></ul>
+    <ul><li>Poder mantenerme a partir de diferentes fuentes de ingresos y poder expandir mis negocios</li></ul>
+</body>
+</html>
