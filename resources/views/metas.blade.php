7b6eab45d3cf0e216173831c22de84177b1ca8ec
Tomas Urieles <tomasurieles31@gmail.com>
2026-02-18 22:26:25 -0500

    estilos css en la carpeta public

--- resources/views/metas.blade.php
+++ resources/views/metas.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sección Metas</title>
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
 </head>
 <body>
     <h1> METAS</h1>
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sección Perfil</title>
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
         <h1>👤 PERFIL</h1>
     <h2>Nombre: Tomás Enrique Urieles Chuscano</h2>
     <h3>Edad: 19</h3>
