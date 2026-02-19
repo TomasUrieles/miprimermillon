57051ab8cd50bb92449aedd9d7d283df8c48efaf
Tomas Urieles <tomasurieles31@gmail.com>
2026-02-18 22:14:23 -0500

    vista habilidades.blade.php creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sección Habilidades</title>
+</head>
+<body>
+    <h1> HABILIDADES</h1>
+    <h3>Técnicas:</h3>
+    <ul>
+    <p>Python</p>
+    </ul>
+    <ul>
+    <p>Kotlin</p>
+    </ul>
+    <ul>
+    <p>Maya</p>
+    </ul>
+    <ul>
+    <p>JavaScript</p>
+    </ul>
+    <h3>Blandas:</h3>
+    <ul>
+    <p>Comunicacion</p>
+    </ul>
+    <ul>
+    <p>Liderazgo</p>
+    </ul>
+    <ul>
+    <p>Empatia</p>
+    </ul>
+</body>
+</html>
